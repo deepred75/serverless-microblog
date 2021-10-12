819cc93a2eb5f4aaa20fd0fc7975cb119a9aba22
Ronny Afriansyah <adrenal999@gmail.com>
2021-10-13 02:06:53 +0700

    Delete microblog directory

--- microblog/_nuxt/static/1634009501/logout/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmicroblog\u002F_nuxt\u002Fstatic\u002F1634009501",layout:"default",error:a,state:{auth:a},serverRendered:true,routePath:"\u002Flogout",config:{_app:{basePath:"\u002Fmicroblog\u002F",assetsPath:"\u002Fmicroblog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
