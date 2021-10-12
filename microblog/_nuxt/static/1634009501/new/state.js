6267125ba9125428f5c87c7a383c0d447b9691bc
Ronny Afriansyah <adrenal999@gmail.com>
2021-10-12 11:15:18 +0700

    Add files via upload

---
+++ microblog/_nuxt/static/1634009501/new/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmicroblog\u002F_nuxt\u002Fstatic\u002F1634009501",layout:"admin",error:a,state:a,serverRendered:false,routePath:"\u002Fnew",config:{_app:{basePath:"\u002Fmicroblog\u002F",assetsPath:"\u002Fmicroblog\u002F_nuxt\u002F",cdnURL:a}}}}(null));
